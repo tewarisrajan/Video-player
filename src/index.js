7a8f60f5f671cf2f93a69d4fcb95de2438ef5b42
Srajan Tewari <tewarisrajan@gmail.com>
2026-07-11 18:33:47 +0530

    Utilities

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ dotenv.config()
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connect failed !!", err);
+    
+})
 
 
 
