d8014f10bf14960c7621d880872771faf0c60654
Srajan Tewari <tewarisrajan@gmail.com>
2026-07-14 16:18:05 +0530

    router and controller changes

--- src/app.js
+++ src/app.js
@@ -15,7 +15,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from "./routes/user.routes.js"
 
 
+//routes 
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants.js"
 import connectDB from "./db/index.js";
-import express from "express"
+import { app } from "./app.js"
 
-dotenv.config()
-
-
-const app = express()
+dotenv.config({
+    path: './.env'
+})
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
