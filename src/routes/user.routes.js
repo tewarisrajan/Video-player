e195498338438c14c6a56097df4e04dc166409b5
Srajan Tewari <tewarisrajan@gmail.com>
2026-07-15 17:30:29 +0530

    auth middelware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -14,6 +15,10 @@ router.post("/register",upload.fields([
         maxCount: 1 
     }
 ]), registerUser)
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 
