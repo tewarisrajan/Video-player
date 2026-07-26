fad58d751bbf73d9a98f0927a84d255e5a50d9e7
Srajan Tewari <tewarisrajan@gmail.com>
2026-07-26 19:06:22 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -377,6 +377,59 @@ const getUserChannelProfile = asyncHandler( async (req, res)=>{
     )
 })
 
+const getWatchHistory = asyncHandler(async(req, res)=>{
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "video",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json
+        (new ApiResponse
+            (200, 
+            user[0].watchHistory),
+            "Watch History fetched successfully")
+})
+
 
 
 export { 
@@ -389,5 +442,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverimage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
  } 
